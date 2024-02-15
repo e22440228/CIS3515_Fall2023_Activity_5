ceaafccb823a648ebb76789e9141809bac06bad1
e22440228 <e12123484@gmail.com>
2024-02-15 12:05:36 -0500

    create callback variable in MainActivity

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
-//        var callback = {number : Int -> displayTextView.textSize = number.toFloat()}
+        var callback = {number : Int -> displayTextView.textSize = number.toFloat()}
         // Todo (Step 3: Pass lambda to adapter)
         recyclerView.adapter = NumberDisplayAdapter(numbers/*, callback*/)
 
